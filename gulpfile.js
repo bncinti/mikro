5b573457a2b598a5620a4ab501a6558c20b6b3ed
Ban Cinti <bncincia@gmail.com>
2021-10-14 11:21:32 +0200

    TS minifikalas es html masolas kesz

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const {src, dest, series} = require('gulp');
+const ts = require('gulp-typescript');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+
+function streamTS(){
+    return src('src/**/*.ts')
+    .pipe(ts()).js
+    .pipe(uglify())
+    .pipe(rename({extname: '.min.js'}))
+    .pipe(dest('public'));
+}
+
+function streamHTML(){
+    return src('src/*.html')
+    .pipe(dest('public'));
+}
+
+exports.html = streamHTML;
+exports.default = series(streamTS,streamHTML);
---
+++ src/js/app.ts
@@ -0,0 +1,3 @@
+console.log('Működik');
+console.log('Működik');
+console.log('Működik');
